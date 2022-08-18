d1c5d154f312111aea208958eb2d5900a3f055e1
Michael Young <mick.young1981@googlemail.com>
2022-08-18 14:51:54 +0100

    Exclude stuff from coverage that doesn't make sense to cover.

--- src/Functions/Startup.cs
+++ src/Functions/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
@@ -13,6 +14,7 @@ using SFA.DAS.Configuration.AzureTableStorage;
 [assembly: FunctionsStartup(typeof(Startup))]
 namespace SFA.DAS.Apprenticeships.Approvals.EventHandlers.Functions
 {
+    [ExcludeFromCodeCoverage]
     public class Startup : FunctionsStartup
     {
         public override void Configure(IFunctionsHostBuilder builder)
