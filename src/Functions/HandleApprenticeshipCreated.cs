9b9d6dc563b046a3bbe40770112eb96618f43731
dinathain <55487881+dinathain@users.noreply.github.com>
2024-06-25 17:31:21 +0100

    Ensure that non pilot apprenticeships are not created in Apprenticeships

--- src/Functions/HandleApprenticeshipCreated.cs
+++ src/Functions/HandleApprenticeshipCreated.cs
@@ -4,40 +4,44 @@ using SFA.DAS.CommitmentsV2.Messages.Events;
 using SFA.DAS.NServiceBus.AzureFunction.Attributes;
 using System.Threading.Tasks;
 
-namespace SFA.DAS.Apprenticeships.Approvals.EventHandlers.Functions
+namespace SFA.DAS.Apprenticeships.Approvals.EventHandlers.Functions;
+
+public class HandleApprenticeshipCreated
 {
-    public class HandleApprenticeshipCreated
+    private readonly IApprenticeshipService _apprenticeshipService;
+
+    public HandleApprenticeshipCreated(IApprenticeshipService apprenticeshipService)
     {
-        private readonly IApprenticeshipService _apprenticeshipService;
+        _apprenticeshipService = apprenticeshipService;
+    }
 
-        public HandleApprenticeshipCreated(IApprenticeshipService apprenticeshipService)
+    [FunctionName("HandleApprenticeshipCreated")]
+    public async Task Handle([NServiceBusTrigger(Endpoint = QueueNames.ApprenticeshipCreated)] ApprenticeshipCreatedEvent @event)
+    {
+        if (!@event.IsOnFlexiPaymentPilot.GetValueOrDefault())
         {
-            _apprenticeshipService = apprenticeshipService;
+            return;
         }
 
-        [FunctionName("HandleApprenticeshipCreated")]
-        public async Task Handle([NServiceBusTrigger(Endpoint = QueueNames.ApprenticeshipCreated)] ApprenticeshipCreatedEvent @event)
-        {
-            await _apprenticeshipService.CreateApproval(
-                @event.Uln,
-                @event.FirstName,
-                @event.LastName,
-                @event.ApprenticeshipId,
-                @event.ProviderId,
-                @event.AccountId,
-                @event.LegalEntityName,
-                @event.EndDate,
-                @event.TransferSenderId,
-                @event.ApprenticeshipEmployerTypeOnApproval,
-                @event.PriceEpisodes,
-                @event.TrainingCode,
-                @event.DateOfBirth,
-                @event.StartDate,
-                @event.ActualStartDate,
-                @event.IsOnFlexiPaymentPilot,
-                @event.ApprenticeshipHashedId,
-                @event.AccountLegalEntityId,
-                @event.TrainingCourseVersion);
-        }
+        await _apprenticeshipService.CreateApproval(
+            @event.Uln,
+            @event.FirstName,
+            @event.LastName,
+            @event.ApprenticeshipId,
+            @event.ProviderId,
+            @event.AccountId,
+            @event.LegalEntityName,
+            @event.EndDate,
+            @event.TransferSenderId,
+            @event.ApprenticeshipEmployerTypeOnApproval,
+            @event.PriceEpisodes,
+            @event.TrainingCode,
+            @event.DateOfBirth,
+            @event.StartDate,
+            @event.ActualStartDate,
+            @event.IsOnFlexiPaymentPilot,
+            @event.ApprenticeshipHashedId,
+            @event.AccountLegalEntityId,
+            @event.TrainingCourseVersion);
     }
 }
